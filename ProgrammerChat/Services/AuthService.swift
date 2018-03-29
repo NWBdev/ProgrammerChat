af8c680f99b6cdf804f243f763c810135911bd1a
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-03-28 23:15:39 -0700

    created AuthService for loginUser, and registerUser

---
+++ ProgrammerChat/Services/AuthService.swift
@@ -0,0 +1,40 @@
+//
+//  AuthService.swift
+//  ProgrammerChat
+//
+//  Created by Nathaniel Burciaga on 3/28/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    // @escapign for error handling
+    func registerUser(withEmail email: String, andPassword password: String, userCeationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCeationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCeationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            
+            loginComplete(true, nil)
+            //sign in happens on its own
+        }
+    }
+}

5d62a81921c1a26ca800699fde29b4efa53f1e31
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-04-04 21:49:12 -0700

    Created CreatePostVC and Uploading Posts to Firebase

---
+++ ProgrammerChat/Controller/CreatePostVC.swift
@@ -0,0 +1,61 @@
+//
+//  CreatePostVC.swift
+//  ProgrammerChat
+//
+//  Created by Nathaniel Burciaga on 4/4/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        textView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    //Actions
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendBtn.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendBtn.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendBtn.isEnabled = true
+                    print("There was a Error!")
+                }
+            })
+        }
+    }
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
+
+

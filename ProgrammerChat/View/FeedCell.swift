905abe1a824e3b092a1b49d00e81d007ee7e876e
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-04-06 20:40:51 -0700

    created feedCell for FeedVC

---
+++ ProgrammerChat/View/FeedCell.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCell.swift
+//  ProgrammerChat
+//
+//  Created by Nathaniel Burciaga on 4/6/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+    
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+        
+    }
+    
+    
+}
